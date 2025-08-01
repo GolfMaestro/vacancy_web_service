e4b16b2ebf75faebf446987d3e4b5ecc74d41ad4
Vlad Eryn <vlad2004e@gmail.com>
2025-08-01 16:37:52 +0300

    basic configuration, added dependencies

---
+++ src/main/java/org/bygolf/VacancyWebSpringApplication.java
@@ -0,0 +1,11 @@
+package org.bygolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyWebSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyWebSpringApplication.class, args);
+    }
+}
